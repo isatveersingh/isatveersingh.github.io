2e0e638b967ed7628ff6dd20d907189327761094
isatveersingh <developer.satveer@gmail.com>
2021-10-17 21:16:31 +0530

    intro component created using vanilla javascript

--- intro-component-with-signup-form/js/app.js
+++ intro-component-with-signup-form/js/app.js
@@ -1,20 +1,83 @@
-const firstName = document.getElementById("first-name");
-const lastName = document.getElementById("last-name");
+// fetch all elements from html file
+const firstName = document.getElementById("firstname");
+const lastName = document.getElementById("lastname");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 
-function validate(){
-    if(firstName.value.length > 4){
-        if(firstName.value == firstName.value.trim()){
-            console.log("it is ok");
-        } else {
-            console.log("anem slho a");
-        }
+let validate = ()=>{
+    // Get value of all input fields
+    let firstNameVal = firstName.value;
+    let lastNameVal = lastName.value;
+    let emailVal = email.value;
+    let passwordVal = password.value;
+
+    // if any input field is invalid then valFlag will be 0 and function returns false
+    let valFlag = 0;
+    
+    // To validate First Name 
+    if(firstNameVal == ""){
+        setError("fname-error");
+        valFlag = 0;
     } else {
-        console.log("enter a vlaid name");
+        removeError("fname-error");
+        valFlag = 1;
+    }
+    
+    // To validate Last Name
+    if(lastNameVal == ""){
+        setError("lname-error");
+        valFlag = 0;
+    } else {
+        removeError("lname-error");
+        valFlag = 1;
+    }
+    
+    // To validate Email Field in correct format
+    let correctWay = /^[a-z0-9_.-]+@[a-z]+\.[a-z]+$/;
+    if(emailVal == "" || !correctWay.test(emailVal)){
+        setError("email-error");
+        valFlag = 0;
+    } else {
+        removeError("email-error");
+        valFlag = 1;
+    }
+    
+
+    // To validate password field and password must be 8 char long
+    if(passwordVal == "" || passwordVal.length < 8){
+        setError("password-error");
+        valFlag = 0;
+    } else {
+        removeError("password-error");
+        valFlag = 1;
     }
 
+    // If all input fields are valid then return true otherwise return false and form will not be submitted
+    if(valFlag == 1){
+        return true;
+    } else if (valFlag == 0){
+        return false;
+    }
+}
 
 
-    return false;
+// This function shows the error if input field is not valid 
+// Pass error fiels class name as argument in funtion
+function setError(clName){
+    const errors = document.querySelectorAll(`.${clName}`);
+    errors.forEach((item)=>{
+        item.classList.add("active");
+    })
+}
+
+
+// This function removes the error if error exists and input field id valid
+// Pass error fiels class name as argument in funtion
+function removeError(clName){
+    const errors = document.querySelectorAll(`.${clName}`);
+    errors.forEach((item)=>{
+        if(item.classList.contains("active")){
+            item.classList.remove("active");
+        }
+    })
 }
