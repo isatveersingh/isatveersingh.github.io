093e48d009259b31375c910e64f3bb6d6cfb23f1
isatveersingh <developer.satveer@gmail.com>
2022-01-02 18:50:23 +0530

    Personal portfolio updated

--- js/main.js
+++ js/main.js
@@ -1,20 +1,30 @@
-const revealOnScroll = document.querySelectorAll(".reveal-on-scroll");
-const revealFromSide = document.querySelectorAll(".reveal-from-side");
-window.addEventListener("scroll", ()=>{
-    revealOnScroll.forEach((revealEle, revealIndex)=>{
-        if(window.scrollY + window.innerHeight > revealEle.offsetTop + revealEle.offsetHeight + 100){
-            // revealEle.style.backgroundColor = "red";
-            revealEle.style.animation = `reveal 0.5s linear forwards`;
-        }
-    })
+const navMenu = document.querySelector(".nav-links");
+const navOpen = document.querySelector(".menu-open");
+const navClose = document.querySelector(".menu-close");
+const navLinkItem = document.querySelectorAll(".nav-link-item");
 
-    revealFromSide.forEach((revealSide, revealSideIndex)=>{
-        if(window.scrollY + window.innerHeight > revealSide.offsetTop + revealSide.offsetHeight) {
-            if(revealSideIndex % 2 == 0) {
-                revealSide.style.animation = `revealfromright 0.5s linear forwards`;
-            } else {
-                revealSide.style.animation = `revealfromleft 0.5s linear forwards`;
-            }
+navOpen.addEventListener("click", ()=>{
+    if(window.innerWidth < 992) {
+        if(!navMenu.classList.contains("show")) {
+            navMenu.classList.add("show");
         }
+    }
+});
+
+function removeNavmenu() {
+    if(window.innerWidth < 992) {
+        if(navMenu.classList.contains("show")) {
+            navMenu.classList.remove("show");
+        }
+    }
+}
+
+navClose.addEventListener("click", ()=>{
+    removeNavmenu();
+});
+
+navLinkItem.forEach((navLink)=>{
+    navLink.addEventListener("click", ()=>{
+        removeNavmenu();
     })
 })
